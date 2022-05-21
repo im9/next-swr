358eac65e858fd48bd49e4a9cf8d3bdf6136c679
t-nakamura <im9today@me.com>
2022-05-21 11:13:18 +0900

    SWR Example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,29 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage, GetServerSideProps } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import useSWR from "swr";
+import styles from "../styles/Home.module.css";
 
-const Home: NextPage = () => {
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+type Props = {
+  data: {
+    subscribers_count: number;
+    stargazers_count: number;
+    forks_count: number;
+  };
+};
+
+const Home: NextPage<Props> = (props) => {
+  const fallbackData = props.data;
+  const { data, error } = useSWR(
+    "https://api.github.com/repos/vercel/swr",
+    fetcher,
+    { fallbackData }
+  );
+  if (error) return <div>An error has occurred.</div>;
+  if (!data) return <div>Loading...</div>;
+
   return (
     <div className={styles.container}>
       <Head>
@@ -18,38 +38,16 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <p>
+            <strong>👁 {data.subscribers_count}</strong>{" "}
+            <strong>✨ {data.stargazers_count}</strong>{" "}
+            <strong>🍴 {data.forks_count}</strong>
+          </p>
         </div>
       </main>
 
@@ -59,14 +57,26 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const data = await fetcher("https://api.github.com/repos/vercel/swr");
+
+  const props: Props = {
+    data,
+  };
+
+  return {
+    props,
+  };
+};
+
+export default Home;
